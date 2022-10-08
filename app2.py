90a12adab3453410b26455f6a4558bfee0193ed0
Uchannel92 <uchenna_attah@hotmail.com>
2022-10-08 19:00:52 +0100

    Comments
    Explaining what i'm doing

--- app2.py
+++ app2.py
@@ -1,7 +1,9 @@
 from app import Dog
 
+# create instance based on the imported class
 chen = Dog()
 
+# call an instance method based on the class.
 chen.wag_tail()
 print(chen.breed)
 print(chen.eyes)
