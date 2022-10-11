2067a1afadb96b0eafdf8093d3adad5c6dd81c3a
Uchannel92 <uchenna_attah@hotmail.com>
2022-10-11 22:53:08 +0100

    Update app5.py
    changed a few variable names, and added a while loop which prompts a question and calls the Class

--- app5.py
+++ app5.py
@@ -13,16 +13,18 @@ class TextFile:
 			print(f'Filename has been saved: {file}')
 
 	def read_file(self):
-
-		file_to_read = input('Type the filename you want to open: ')
-		filename = f'{file_to_read}.txt'
+		""" read contents of text file entered """
+		filename = input('Type the filename you want to open: ')
+		file_to_read = f'{filename}.txt'
 
 		with open(file_to_read, 'r') as file_object:
 			data = file_object.read()
+			print(data)
 
 	def append_to_file(self):
-		
-		file_to_read = input('Type the filename you want to edit: ')
+		""" write data to text file """
+		question = input('Type the filename you want to edit: ')
+		file_to_read = f'{question}.txt'
 		text = input('Enter the text you want to write: ')
 
 		with open(file_to_read, 'a') as file_object:
@@ -30,12 +32,18 @@ class TextFile:
 			print(f'You entered: {text}')
 
 
-chen_test = TextFile()
-chen_test.create_file()
-chen_test.append_to_file()
-chen_test.read_file()
-
+new_file = TextFile()
 # create while loop which asks the citizen what they want to do.
+while True:
 
+	question = input('To write to file press any key to continue. press "q" to quit')
 
-print('\nGoodbye')
+	if question == 'q':
+		print('\nGoodbye')
+		break
+
+	elif question != 'q':
+
+		new_file.create_file()
+		new_file.append_to_file()
+		new_file.read_file()
